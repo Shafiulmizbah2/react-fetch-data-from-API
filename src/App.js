059b41bbd58b3a17b23f29dd961ce44ae1a006a6
Shafiul Azim <shafiulmizbah2@gmail.com>
2021-03-09 23:47:16 +0600

    Initial fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState , useEffect} from 'react'
+import './App.scss';
+import MarkupPerson from './markupPerson';
 
 function App() {
+  
+  const [persons,setPersons] = useState([]);
+
+  const getData = async function(){
+    const res = await fetch('https://api.github.com/users');
+    const users = await res.json();
+    setPersons(users);
+    //console.log(users);
+  }
+
+  useEffect(()=>{
+    getData();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="heading">Rendering github user from API</h1>
+      <div className="persons">
+        {
+          persons.map( (user,i)=>{
+              return (
+                <MarkupPerson
+                  key = {i}
+                  image = {user.avatar_url}
+                  name = {user.login}
+                  link = { user.html_url}
+                />
+              )
+          })
+        }
+      </div>
+           
     </div>
   );
 }
---
+++ src/markupPerson.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function markupPerson(props) {
+    return (
+       
+        <div className="person">
+            <div className="profile">
+                <img src={props.image} alt={props.name} className="profile-img"></img>
+            </div>
+            <div className="content">
+                <h3 className="heading-tertiary">{props.name}</h3>
+                <a href={props.link} className="btn">Profile</a>
+            </div> 
+        </div>   
+              
+    )
+}
+
